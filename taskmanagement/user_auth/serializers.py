6ca9c1616a60c9883d3ee8069424694c08bc9426
rxAnirudh <radixdt.2336@gmail.com>
2022-07-11 19:13:26 +0530

    🔨 Changes done related to removing hard coded role id and other id changes in user table

--- taskmanagement/user_auth/serializers.py
+++ taskmanagement/user_auth/serializers.py
@@ -6,10 +6,11 @@ from django.contrib.auth import password_validation
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
-        exclude = ["profile_pic","user_id","created_at","updated_at","is_active","is_delete"]
+        exclude = ["profile_pic","user_id","created_at","updated_at","is_active","is_delete","status_id"]
     def validate_password(self, value):
      password_validation.validate_password(value, self.instance)
      return value
+
 
 class SignInSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,3 +26,80 @@ class ResetPasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ['email','password']
+
+class UserUpdateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        exclude = ["profile_pic","user_id","created_at","updated_at","is_active","is_delete"]
+
+
+class DeleteProfileSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = UserModel
+        fields = ['id']
+
+
+class AddUserStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserStatusModel
+        exclude = ["created_at","updated_at","is_active","is_delete"]
+
+
+class GetUserStatusSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(default=None)
+    class Meta:
+        model = UserStatusModel
+        exclude = ["user_status","created_at","updated_at","is_active","is_delete"]
+
+
+class UpdateUserStatusSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = UserStatusModel
+        exclude = ["created_at","updated_at","is_active","is_delete"]
+
+
+class DeleteUserStatusSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = UserStatusModel
+        exclude = ["user_status","created_at","updated_at","is_active","is_delete"]
+
+
+class AddUserRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRoleModel
+        exclude = ["created_at","updated_at","is_active","is_delete"]
+
+
+class GetUserRoleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(default=None)
+    class Meta:
+        model = UserRoleModel
+        exclude = ["user_role","created_at","updated_at","is_active","is_delete"]
+
+
+class UpdateUserRoleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = UserRoleModel
+        exclude = ["created_at","updated_at","is_active","is_delete"]
+
+
+class DeleteUserRoleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = UserRoleModel
+        exclude = ["user_role","created_at","updated_at","is_active","is_delete"]
+
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField()
+    new_password = serializers.CharField()
+    class Meta:
+        model = UserModel
+        fields = ['user_id','old_password','new_password']
+    def validate_password(self, value):
+     password_validation.validate_password(value, self.instance)
+     return value
--- taskmanagement/user_auth/urls.py
+++ taskmanagement/user_auth/urls.py
@@ -6,5 +6,20 @@ urlpatterns = [
 	path('signup/', views.signup, name="signup"),
     path('signin/', views.signin, name="signin"),
     path('forgotpassword/', views.forgotpassword, name="forgotpassword"),
-	path('resetpassword/', views.resetpassword, name="resetpassword")
+	path('resetpassword/', views.resetpassword, name="resetpassword"),
+    	path('forgotpassword/', views.forgotpassword, name="forgotpassword"),
+	path('resetpassword/', views.resetpassword, name="resetpassword"),
+	path('changepassword/', views.changepassword, name="changepassword"),
+	path('signin/', views.signin, name="signin"),
+	path('deleteprofile/',views.deleteprofile,name="deleteprofile"),
+	path('updateprofile/',views.updateprofile,name="updateprofile"),
+	path('adduserrole/',views.adduserrole,name="adduserrole"),
+	path('updateuserrole/',views.updateuserrole,name="updateuserrole"),
+	path('deleteuserrole/',views.deleteuserrole,name="deleteuserrole"),
+	path('getuserrole/',views.getuserrole,name="getuserrole"),
+	path('adduserstatus/',views.adduserstatus,name="adduserstatus"),
+	path('updateuserstatus/',views.updateuserstatus,name="updateuserstatus"),
+	path('deleteuserstatus/',views.deleteuserstatus,name="deleteuserstatus"),
+	path('getuserstatus/',views.getuserstatus,name="getuserstatus"),
+
 ]
